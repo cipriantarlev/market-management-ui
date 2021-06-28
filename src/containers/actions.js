76141090b56892290971818ede615122d3002fcd
cipriantarlev <ciprian.tarlev@gmail.com>
2021-06-28 16:36:17 +0300

    MM-14: Create User component. CRUD operations: done

--- src/containers/actions.js
+++ src/containers/actions.js
@@ -1,5 +1,6 @@
 import { 
   authorizationData,
+  dataApi,
   ROOT_CONTEXT_PATH,
   REQUEST_LOGIN_PENDING,
   REQUEST_LOGIN_SUCCESS,
@@ -13,7 +14,16 @@ import {
   REQUEST_USER_FAILED,
   REQUEST_ROLES_PENDING,
   REQUEST_ROLES_SUCCESS,
-  REQUEST_ROLES_FAILED
+  REQUEST_ROLES_FAILED,
+  CREATE_USER_PENDING,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILED,
+  UPDATE_USER_PENDING,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILED,
+  DELETE_USER_PENDING,
+  DELETE_USER_SUCCESS,
+  DELETE_USER_FAILED,
 } from './constants';
 
 export const handleLogin = (username, password) => (dispatch) => {
@@ -52,6 +62,29 @@ export const fetchUser = (id) => (dispatch) => {
   .catch(error => dispatch({ type: REQUEST_USER_FAILED, payload: error }))
 }
 
+export const createUser = (user) => (dispatch) => {
+  dispatch({ type: CREATE_USER_PENDING});
+  fetch(`${ROOT_CONTEXT_PATH}/users`, dataApi('post', user))
+  .then(respone => respone.json())
+  .then(data => dispatch({ type: CREATE_USER_SUCCESS, payload: data}))
+  .catch(error => dispatch({ type: CREATE_USER_FAILED, payload: error }))
+}
+
+export const updateUser = (user) => (dispatch) => {
+  dispatch({ type: UPDATE_USER_PENDING});
+  fetch(`${ROOT_CONTEXT_PATH}/users`, dataApi('put', user))
+  .then(respone => respone.json())
+  .then(data => dispatch({ type: UPDATE_USER_SUCCESS, payload: data}))
+  .catch(error => dispatch({ type: UPDATE_USER_FAILED, payload: error }))
+}
+
+export const deleteUser = (id) => (dispatch) => {
+  dispatch({ type: DELETE_USER_PENDING});
+  fetch(`${ROOT_CONTEXT_PATH}/users/${id}`, dataApi('delete'))
+  .then(respone => dispatch({ type: DELETE_USER_SUCCESS, payload: respone.status}))
+  .catch(error => dispatch({ type: DELETE_USER_FAILED, payload: error }))
+}
+
 export const fetchRoles = () => (dispatch) => {
   dispatch({ type: REQUEST_ROLES_PENDING});
   fetch(`${ROOT_CONTEXT_PATH}/roles`, authorizationData())
@@ -59,3 +92,5 @@ export const fetchRoles = () => (dispatch) => {
   .then(data => dispatch({ type: REQUEST_ROLES_SUCCESS, payload: data}))
   .catch(error => dispatch({ type: REQUEST_ROLES_FAILED, payload: error}))
 }
+
+
