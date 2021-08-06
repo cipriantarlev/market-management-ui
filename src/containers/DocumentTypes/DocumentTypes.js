fefefe699f056443dbb64a9285e35a70dc59bf9a
cipriantarlev <ciprian.tarlev@gmail.com>
2021-08-06 12:41:45 +0300

    MM-40: Document Type Component is created

---
+++ src/containers/DocumentTypes/DocumentTypes.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
+import { fetchDocumentTypes, deleteDocumentType } from '../actions';
+import DocumentType from './DocumentType';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
+
+const mapStateToProps = (state) => {
+  return {
+    isPending: state.manageDocumentTypes.isPending,
+    documentTypes: state.manageDocumentTypes.documentTypes,
+    error: state.manageDocumentTypes.error,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchDocumentTypes: () => dispatch(fetchDocumentTypes()),
+    onDeleteDocumentType: (id) => dispatch(deleteDocumentType(id)),
+  }
+}
+
+const DocumentTypes = (props) => {
+  const {
+    isPending,
+    documentTypes,
+    error,
+    onFetchDocumentTypes,
+    onDeleteDocumentType
+  } = props;
+
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [id, setId] = useState(0);
+
+  const [totalRows, setTotalRows] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+
+  const handleClose = (event, reason) => {
+    if (reason !== "backdropClick") {
+      setOpenDialog(false);
+    }
+  };
+
+  useEffect(() => {
+    onFetchDocumentTypes();
+  }, [onFetchDocumentTypes])
+
+  useEffect(() => {
+    setTotalRows(documentTypes.length);
+  }, [documentTypes])
+
+  const onAddNewDocumentType = () => {
+    setId(0);
+    setOpenDialog(true);
+  }
+
+  const onUpdateDocumentType = (elementId) => {
+    setId(elementId);
+    setOpenDialog(true);
+  }
+
+  const removeDocumentType = (documentTypeId) => {
+    const answer = window.confirm(`Are you sure you want to delete the Document Type with id: ${documentTypeId}?`);
+    if (answer === true) {
+      onDeleteDocumentType(documentTypeId);
+      history.go(0);
+    }
+  }
+
+  const changePage = (event, pageNr) => {
+    if(pageNr <= totalRows){
+      setPage(pageNr)
+    }
+  }
+
+  return (
+    <div style={{ width: 500, margin: 'auto', marginTop: 30 }}>
+      <Button
+        variant="contained"
+        className="mb-4"
+        style={{ backgroundColor: '#2aa839', color: 'white' }}
+        startIcon={<AddCircleOutlineIcon />}
+        onClick={onAddNewDocumentType}
+      >
+        Add new Document Type
+      </Button>
+      {!isPending ?
+        <div>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} size="small" aria-label="simple table">
+              <TableHead style={{ backgroundColor: "#808080ad" }}>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell >Name</TableCell>
+                  <TableCell align="center">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {documentTypes
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((element) => (
+                  <TableRow key={element.id}>
+                    <TableCell component="th" scope="row">
+                      <Link className="no-underline" to="#" onClick={() => onUpdateDocumentType(element.id)} >
+                        {element.id}
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      <Link className="no-underline" to="#" onClick={() => onUpdateDocumentType(element.id)}>
+                        {element.name}
+                      </Link>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        startIcon={<DeleteIcon />}
+                        onClick={() => removeDocumentType(element.id)}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    rowsPerPageOptions={[10, 20, 50]}
+                    rowsPerPage={rowsPerPage}
+                    count={totalRows}
+                    page={page}
+                    onChangePage={changePage}
+                    onChangeRowsPerPage={(event) => setRowsPerPage(event.target.value)}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </TableContainer>
+          <DocumentType
+            open={openDialog}
+            handleClose={handleClose}
+            id={id}
+          />
+        </div>
+        :
+        <h3>Loading data ...</h3>}
+      {error ? <div style={{ color: 'red', textAlign: 'center', margin: 20, fontSize: '2em' }}>Error! Something went wrong!!!</div> : null}
+    </div>
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DocumentTypes);
--- src/containers/actions.js
+++ src/containers/actions.js
@@ -144,6 +144,21 @@ import {
   GENERATE_BARCODE_PENDING,
   GENERATE_BARCODE_SUCCESS,
   GENERATE_BARCODE_FAILED,
+  REQUEST_DOCUMENT_TYPES_PENDING,
+  REQUEST_DOCUMENT_TYPES_SUCCESS,
+  REQUEST_DOCUMENT_TYPES_FAILED,
+  REQUEST_DOCUMENT_TYPE_PENDING,
+  REQUEST_DOCUMENT_TYPE_SUCCESS,
+  REQUEST_DOCUMENT_TYPE_FAILED,
+  CREATE_DOCUMENT_TYPE_PENDING,
+  CREATE_DOCUMENT_TYPE_SUCCESS,
+  CREATE_DOCUMENT_TYPE_FAILED,
+  UPDATE_DOCUMENT_TYPE_PENDING,
+  UPDATE_DOCUMENT_TYPE_SUCCESS,
+  UPDATE_DOCUMENT_TYPE_FAILED,
+  DELETE_DOCUMENT_TYPE_PENDING,
+  DELETE_DOCUMENT_TYPE_SUCCESS,
+  DELETE_DOCUMENT_TYPE_FAILED,
 } from './constants';
 
 export const handleLogin = (username, password) => (dispatch) => {
@@ -524,4 +539,43 @@ export const generateBarcode = (barcode) => (dispatch) => {
     .then(response => response.json())
     .then(data => dispatch({ type: GENERATE_BARCODE_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: GENERATE_BARCODE_FAILED, payload: error }))
+}
+
+export const fetchDocumentTypes = () => (dispatch) => {
+  dispatch({ type: REQUEST_DOCUMENT_TYPES_PENDING });
+  fetch(`${ROOT_CONTEXT_PATH}/document-types`, authorizationData())
+    .then(response => response.json())
+    .then(data => dispatch({ type: REQUEST_DOCUMENT_TYPES_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_DOCUMENT_TYPES_FAILED, payload: error }))
+}
+
+export const fetchDocumentType = (id) => (dispatch) => {
+  dispatch({ type: REQUEST_DOCUMENT_TYPE_PENDING });
+  fetch(`${ROOT_CONTEXT_PATH}/document-types/${id}`, authorizationData())
+    .then(response => response.json())
+    .then(data => dispatch({ type: REQUEST_DOCUMENT_TYPE_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_DOCUMENT_TYPE_FAILED, payload: error }))
+}
+
+export const createDocumentType = (documentType) => (dispatch) => {
+  dispatch({ type: CREATE_DOCUMENT_TYPE_PENDING });
+  fetch(`${ROOT_CONTEXT_PATH}/document-types`, dataApi('post', documentType))
+    .then(response => response.json())
+    .then(data => dispatch({ type: CREATE_DOCUMENT_TYPE_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: CREATE_DOCUMENT_TYPE_FAILED, payload: error }))
+}
+
+export const updateDocumentType = (documentType) => (dispatch) => {
+  dispatch({ type: UPDATE_DOCUMENT_TYPE_PENDING });
+  fetch(`${ROOT_CONTEXT_PATH}/document-types`, dataApi('put', documentType))
+    .then(response => response.json())
+    .then(data => dispatch({ type: UPDATE_DOCUMENT_TYPE_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: UPDATE_DOCUMENT_TYPE_FAILED, payload: error }))
+}
+
+export const deleteDocumentType = (id) => (dispatch) => {
+  dispatch({ type: DELETE_DOCUMENT_TYPE_PENDING });
+  fetch(`${ROOT_CONTEXT_PATH}/document-types/${id}`, dataApi('delete'))
+    .then(respone => dispatch({ type: DELETE_DOCUMENT_TYPE_SUCCESS, payload: respone.status }))
+    .catch(error => dispatch({ type: DELETE_DOCUMENT_TYPE_FAILED, payload: error }))
 }
