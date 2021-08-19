135878403c0a47e02b1c734c05e016b87ae40b32
cipriantarlev <ciprian.tarlev@gmail.com>
2021-08-19 18:38:14 +0300

    MM-42: Added update functionality and also find product by barcode

--- src/containers/InvoiceProducts/InvoiceProduct.js
+++ src/containers/InvoiceProducts/InvoiceProduct.js
@@ -22,7 +22,7 @@ import './style.css';
 const mapStateToProps = (state) => {
   return {
     isPending: state.manageInvoiceProducts.isPending,
-    initialInvoiceProducts: state.manageInvoiceProducts.invoiceProducts,
+    initialInvoiceProduct: state.manageInvoiceProducts.invoiceProduct,
     error: state.manageInvoiceProducts.error,
     measuringUnits: state.manageMeasuringUnits.measuringUnits,
     productByBarcode: state.manageProducts.productByBarcode,
@@ -42,31 +42,50 @@ const mapDispatchToProps = (dispatch) => {
 const InvoiceProduct = (props) => {
   const {
     isPending,
-    initialInvoiceProducts,
+    initialInvoiceProduct,
     error,
     measuringUnits,
-    // productByBarcode,
+    productByBarcode,
     onFetchInvoiceProduct,
     onCreateInvoiceProduct,
     onUpdateInvoiceProduct,
     onFetchMeasuringUnits,
-    onFetchInvoiceProducts,
-    // onFetchProductByBarcode,
+    // onFetchInvoiceProducts,
+    onFetchProductByBarcode,
   } = props;
 
   let history = useHistory();
   const { id } = useParams();
 
   const [invoiceProduct, setInvoiceProduct] = useState({});
+  const [invoice, setInvoice] = useState({});
+  const [product, setProduct] = useState({});
   const [selectedMeasuringUnit, setSelectedMeasuringUnit] = useState(0);
+
+  const [vendorPrice, setVendorPrice] = useState(0);
+  const [sum, setSum] = useState(0);
+  const [vatSumProduct, setVatSum] = useState(0);
+  const [tradeMarginProduct, setTradeMargin] = useState(0);
+  const [retailPrice, setRetailPrice] = useState(0);
 
   const onClickCancel = () => {
     const answer = window.confirm('Are you sure you want to cancel?');
     if (answer === true) {
       setInvoiceProduct({});
+      setInvoice({});
+      setProduct({});
       setSelectedMeasuringUnit(0);
+      setVendorPrice(0);
+      setSum(0);
+      setVatSum(0);
+      setTradeMargin(0);
+      setRetailPrice(0);
       history.goBack();
     }
+  }
+
+  const addNewProduct = () => {
+    history.push("/products/0")
   }
 
   useEffect(() => {
@@ -79,42 +98,132 @@ const InvoiceProduct = (props) => {
     }
   }, [id, onFetchInvoiceProduct])
 
+  useEffect(() => {
+    setProduct(productByBarcode);
+    intializeMeasuringUnitValue();
+    intializeVendorPriceValue();
+    intializeTradeMarginValue();
+    intializeRetailPriceValue();
+    // eslint-disable-next-line
+  }, [productByBarcode])
+
   const intializeMeasuringUnitValue = () => {
-    if (initialInvoiceProducts.measuringUnit !== undefined) {
-      setSelectedMeasuringUnit(initialInvoiceProducts.measuringUnit.id)
+    if (initialInvoiceProduct?.product?.measuringUnit !== undefined && id !== "0") {
+      setSelectedMeasuringUnit(initialInvoiceProduct?.product?.measuringUnit.id)
+    }
+    if (productByBarcode?.measuringUnit !== undefined) {
+      setSelectedMeasuringUnit(productByBarcode?.measuringUnit.id)
+    }
+  }
+
+  const intializeVendorPriceValue = () => {
+    if (initialInvoiceProduct?.product?.discountPrice !== undefined && id !== "0") {
+      setVendorPrice(initialInvoiceProduct?.product?.discountPrice)
+    }
+    if (productByBarcode?.discountPrice !== undefined) {
+      setVendorPrice(productByBarcode?.discountPrice)
+    }
+  }
+
+  const intializeSumValue = () => {
+    if (initialInvoiceProduct?.totalDiscountPrice !== undefined && id !== "0") {
+      setSum(initialInvoiceProduct?.totalDiscountPrice)
+    }
+  }
+
+  const intializeVatSumValue = () => {
+    if (initialInvoiceProduct?.vatSum !== undefined && id !== "0") {
+      setVatSum(initialInvoiceProduct?.vatSum)
+    }
+  }
+
+  const intializeTradeMarginValue = () => {
+    if (initialInvoiceProduct?.product?.tradeMargin !== undefined && id !== "0") {
+      setTradeMargin(initialInvoiceProduct?.product?.tradeMargin)
+    }
+    if (productByBarcode?.tradeMargin !== undefined) {
+      setTradeMargin(productByBarcode?.tradeMargin)
+    }
+  }
+
+  const intializeRetailPriceValue = () => {
+    if (initialInvoiceProduct?.product?.retailPrice !== undefined && id !== "0") {
+      setRetailPrice(initialInvoiceProduct?.product?.retailPrice)
+    }
+    if (productByBarcode?.retailPrice !== undefined) {
+      setRetailPrice(productByBarcode?.retailPrice)
     }
   }
 
   useEffect(() => {
     if (id !== "0") {
-      setInvoiceProduct(initialInvoiceProducts)
+      setInvoiceProduct(initialInvoiceProduct);
+      setInvoice(initialInvoiceProduct?.invoice);
+      setProduct(initialInvoiceProduct?.product);
       intializeMeasuringUnitValue();
-    } // eslint-disable-next-line
-  }, [initialInvoiceProducts, id])
+      intializeVendorPriceValue();
+      intializeSumValue();
+      intializeVatSumValue();
+      intializeTradeMarginValue();
+      intializeRetailPriceValue();
+    }// eslint-disable-next-line
+  }, [initialInvoiceProduct, id])
+
+  const displayNnumberWith2Decimals = (numberTodisplay) => {
+    if (isNaN(numberTodisplay)) {
+      return Number(0).toFixed(2);
+    } else {
+      return Number(numberTodisplay).toFixed(2);
+    }
+  }
+
+  const displayQuantityValueBasedOnMeasuringUnit = (numberTodisplay) => {
+    if (isNaN(numberTodisplay)) {
+      return ""
+    } else {
+      if (product?.measuringUnit.id === 1) {
+        return Number(numberTodisplay).toFixed(4);
+      } else if (product?.measuringUnit.id === 2) {
+        return Number(numberTodisplay);
+      }
+    }
+  }
 
   const onChangeInvoiceProductValues = (event) => {
     switch (event.target.id) {
-      case "formGridDocumentType":
-        let measuringUnitObj = getDropDownValue(Number(event.target.value), selectedMeasuringUnit, setSelectedMeasuringUnit);
-        setInvoiceProduct({ ...invoiceProduct, measuringUnit: measuringUnitObj });
+      case "formGridQuantity":
+        if (vendorPrice !== null || vendorPrice !== undefined) {
+          setSum(vendorPrice * Number(event.target.value));
+          setVatSum(vendorPrice * Number(event.target.value) * (product?.vat?.value / 100))
+        }
+        setInvoiceProduct({ ...invoiceProduct, quantity: Number(event.target.value) });
         break;
-      case "formGridOrganization":
-
+      case "formGridVendorPrice":
+        console.log()
+        if (invoiceProduct?.quantity !== null || invoiceProduct?.quantity !== undefined) {
+          setSum(invoiceProduct?.quantity * Number(event.target.value));
+          setVatSum(invoiceProduct?.quantity * Number(event.target.value) * (product?.vat?.value / 100))
+        }
+        setVendorPrice(Number(event.target.value));
         break;
-      case "formGridVendor":
-
+      case "formGridSum":
+        if (invoiceProduct?.quantity !== null || invoiceProduct?.quantity !== undefined) {
+          setVendorPrice(Number(event.target.value) / invoiceProduct?.quantity);
+          setVatSum(Number(event.target.value) * (product?.vat?.value / 100))
+        }
+        setSum(Number(event.target.value));
         break;
-      case "formGridDateCreated":
-        setInvoiceProduct({ ...invoiceProduct, dateCreated: event.target.value });
+      case "formGridRetailPrice":
+        if (vendorPrice !== null || vendorPrice !== undefined) {
+          setTradeMargin(((Number(event.target.value) * 100) / vendorPrice - 100))
+        }
+        setRetailPrice(Number(event.target.value));
         break;
-      case "formGridInvoiceNumber":
-        setInvoiceProduct({ ...invoiceProduct, invoiceNumber: event.target.value });
-        break;
-      case "formGridInvoiceDate":
-        setInvoiceProduct({ ...invoiceProduct, invoiceDate: event.target.value });
-        break;
-      case "formGridNote":
-        setInvoiceProduct({ ...invoiceProduct, note: event.target.value });
+      case "formGridTradeMarginPercent":
+        if (vendorPrice !== null || vendorPrice !== undefined) {
+          setRetailPrice(vendorPrice * (Number(event.target.value) / 100 + 1))
+        }
+        setTradeMargin(Number(event.target.value));
         break;
       default:
         setInvoiceProduct(invoiceProduct);
@@ -122,21 +231,49 @@ const InvoiceProduct = (props) => {
     }
   }
 
-  const getDropDownValue = (itemId, itemList, setItem) => {
-    setItem(itemId);
-    return itemList.find(item => item.id === itemId);
+  const onPressEnterBarcodeField = (event) => {
+    if (event.keyCode === 13 || event.keyCode === 9) {
+      onFetchProductByBarcode(event.target.value)
+    }
+  }
+
+  console.log("productByBarcode", productByBarcode);
+
+  const prepareInvoiceProductForSubmit = () => {
+    Object.assign(product, product, {
+      discountPrice: vendorPrice,
+      retailPrice: retailPrice,
+      tradeMargin: tradeMarginProduct,
+      stock: invoiceProduct?.quantity
+    })
+
+    Object.assign(invoiceProduct, invoiceProduct, {
+      invoice: invoice,
+      product: product,
+      totalDiscountPrice: sum,
+      totalRetailPrice: retailPrice * invoiceProduct?.quantity,
+      vatSum: vatSumProduct
+    })
   }
 
   const onSubmitInvoiceProduct = () => {
+    prepareInvoiceProductForSubmit();
+    console.log("invoiceProduct", invoiceProduct);
     if (id !== "0") {
       onUpdateInvoiceProduct(invoiceProduct);
     } else {
       onCreateInvoiceProduct(invoiceProduct);
     }
-    history.goBack();
-    onFetchInvoiceProducts();
+    history.goBack();;
     setInvoiceProduct({});
+    setInvoice({});
+    setProduct({});
     setSelectedMeasuringUnit(0);
+    setVendorPrice(0);
+    setSum(0);
+    setVatSum(0);
+    setTradeMargin(0);
+    setRetailPrice(0);
   }
 
   return (
@@ -160,11 +297,11 @@ const InvoiceProduct = (props) => {
                   type="text"
                   size="sm"
                   disabled
+                  readOnly
                   style={{
                     textAlign: 'right'
                   }}
-                  value={1111}
-                  onChange={onChangeInvoiceProductValues}
+                  value={invoice?.id}
                 />
               </Col>
             </Form.Group>
@@ -180,10 +317,10 @@ const InvoiceProduct = (props) => {
               <Col sm="9">
                 <Form.Control
                   type="text"
-                  placeholder="Enter Barcode"
+                  placeholder="Find by Barcode"
                   size="sm"
-                  // value={invoice.invoiceNumber}
-                  onChange={onChangeInvoiceProductValues}
+                  value={invoiceProduct?.product?.barcodes[0].value}
+                  onKeyDown={onPressEnterBarcodeField}
                 />
               </Col>
             </Form.Group>
@@ -200,39 +337,40 @@ const InvoiceProduct = (props) => {
                 <Form.Control
                   type="text"
                   size="sm"
-                  // value={invoice.invoiceNumber}
-                  onChange={onChangeInvoiceProductValues}
+                  disabled
+                  readOnly
+                  value={product?.productCode?.value}
                 />
               </Col>
             </Form.Group>
-                <Form.Group as={Row} controlId="formGridProductName">
-                <Form.Label
-                    column
-                    sm="3"
-                    style={{
-                      paddingTop: 0,
-                      paddingBottom: 0
-                    }}
-                  >Product Name</Form.Label>
-                  <Col sm="8">
-                    <Form.Control
-                      type="text"
-                      size="sm"
-                      // value={invoice.invoiceNumber}
-                      onChange={onChangeInvoiceProductValues}
-                    />
-                  </Col>
-                  <Col sm="1">
-                    <Form.Control
-                      type="button"
-                      size="sm"
-                      className="form-button"
-                      value={"+"}
-                      onClick={() => alert('hello')}
-                    />
-                  </Col>
-                </Form.Group>
-                
+            <Form.Group as={Row} controlId="formGridProductName">
+              <Form.Label
+                column
+                sm="3"
+                style={{
+                  paddingTop: 0,
+                  paddingBottom: 0
+                }}
+              >Product Name</Form.Label>
+              <Col sm="8">
+                <Form.Control
+                  type="text"
+                  size="sm"
+                  placeholder="Find by Name"
+                  value={product?.nameRom}
+                />
+              </Col>
+              <Col sm="1">
+                <Form.Control
+                  type="button"
+                  size="sm"
+                  className="form-button"
+                  value={"+"}
+                  onClick={addNewProduct}
+                />
+              </Col>
+            </Form.Group>
+
             <Row>
               <Col>
                 <Form.Group as={Row} controlId="formGridQuantity">
@@ -251,20 +389,21 @@ const InvoiceProduct = (props) => {
                       style={{
                         textAlign: 'right'
                       }}
-                      // value={invoice.invoiceNumber}
+                      value={displayQuantityValueBasedOnMeasuringUnit(invoiceProduct?.quantity)}
                       onChange={onChangeInvoiceProductValues}
                     />
                   </Col>
                 </Form.Group>
               </Col>
               <Col>
-                <Form.Group as={Row} controlId="formGridMeasuringUnti">
+                <Form.Group as={Row} controlId="formGridMeasuringUnt">
                   <Col sm="4">
                     <Form.Control
                       as="select"
                       size="sm"
+                      disabled
+                      readOnly
                       value={selectedMeasuringUnit}
-                      onChange={onChangeInvoiceProductValues}
                     >
                       {measuringUnits.map(item => (
                         <option key={item.id} value={item.id}>{item.name}</option>
@@ -292,7 +431,7 @@ const InvoiceProduct = (props) => {
                       style={{
                         textAlign: 'right'
                       }}
-                      // value={invoice.invoiceNumber}
+                      value={displayNnumberWith2Decimals(vendorPrice)}
                       onChange={onChangeInvoiceProductValues}
                     />
                   </Col>
@@ -315,7 +454,7 @@ const InvoiceProduct = (props) => {
                       style={{
                         textAlign: 'right'
                       }}
-                      // value={invoice.invoiceNumber}
+                      value={displayNnumberWith2Decimals(sum)}
                       onChange={onChangeInvoiceProductValues}
                     />
                   </Col>
@@ -335,11 +474,12 @@ const InvoiceProduct = (props) => {
                 <Form.Control
                   type="text"
                   size="sm"
+                  disabled
+                  readOnly
                   style={{
                     textAlign: 'right'
                   }}
-                  // value={invoice.invoiceNumber}
-                  onChange={onChangeInvoiceProductValues}
+                  value={displayNnumberWith2Decimals(vatSumProduct)}
                 />
               </Col>
             </Form.Group>
@@ -361,7 +501,7 @@ const InvoiceProduct = (props) => {
                       style={{
                         textAlign: 'right'
                       }}
-                      // value={invoice.invoiceNumber}
+                      value={displayNnumberWith2Decimals(retailPrice)}
                       onChange={onChangeInvoiceProductValues}
                     />
                   </Col>
@@ -384,7 +524,7 @@ const InvoiceProduct = (props) => {
                       style={{
                         textAlign: 'right'
                       }}
-                      // value={invoice.note}
+                      value={displayNnumberWith2Decimals(tradeMarginProduct)}
                       onChange={onChangeInvoiceProductValues}
                     />
                   </Col>
@@ -405,8 +545,8 @@ const InvoiceProduct = (props) => {
                   type="text"
                   size="sm"
                   disabled
-                  // value={invoice.note}
-                  onChange={onChangeInvoiceProductValues}
+                  readOnly
+                  value={displayQuantityValueBasedOnMeasuringUnit(product?.stock)}
                 />
               </Col>
             </Form.Group>
